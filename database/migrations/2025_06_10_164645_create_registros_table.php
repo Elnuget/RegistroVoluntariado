366b40523a00d7ff0b10acc1d209d304d8624082
Elnuget <cangulo009@outlook.es>
2025-06-11 11:14:03 -0500

    arreglo migracion

--- database/migrations/2025_06_10_164645_create_registros_table.php
+++ database/migrations/2025_06_10_164645_create_registros_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->unsignedBigInteger('voluntario_id');
             $table->date('fecha');
             $table->time('hora');
-            $table->string('tipo');
+            $table->string('tipo_actividad')->nullable();
             $table->string('ubicacion_desde');
             $table->string('ubicacion_hasta');
             $table->decimal('millas', 8, 2)->nullable();
